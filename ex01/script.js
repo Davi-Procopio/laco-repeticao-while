d91eac17a026c3de0ee1b201f3a0a1fbf1fb039c
Davi-Procopio <daviprocopio1234567@gmail.com>
2026-05-06 08:30:04 -0300

    VALIDAÇÃO POSITIVO/NEGATIVO

---
+++ ex01/script.js
@@ -0,0 +1,6 @@
+let numero = Number(prompt("Digite um número positivo:"));
+while (numero < 0) {
+    numero = Number(prompt("Para de ser burro e escreve um número positivo!"));
+} if (numero >= 0) {
+    alert ("Parabéns, fez o mínimo!")
+} 
